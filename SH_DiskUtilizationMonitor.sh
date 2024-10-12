6bd8f7935fb7364a6ebb2040db3db9be09548844
Automation-Crazz <adityaarun48@gmail.com>
2024-10-12 21:18:44 +0530

    Update SH_DiskUtilizationMonitor.sh
    The following code is responsible to capture the Disk utilization on various list of linux servers and creating a HTML file.

--- SH_DiskUtilizationMonitor.sh
+++ SH_DiskUtilizationMonitor.sh
@@ -1,3 +1,7 @@
+# The following code is responsible to capture the Disk utilization on various list of linux servers and creating a HTML file.
+# The list of servers needs to be provided in /var/autoheal/housekeeping/servers.txt
+
+
 #!/bin/bash
 
  
@@ -13,7 +17,14 @@ function check_server {
     #status=$(timeout 30s ssh -i "$private_key" -q -o BatchMode=yes -o ConnectTimeout=5 "$hostname" exit && echo "OK" || echo "Failed")
     status=$(ssh -q -o BatchMode=yes -o ConnectTimeout=5 "$hostname" exit && echo "OK" || echo "Failed")
     if [ "$status" == "OK" ]; then
-        echo "<tr><td style='height: 40px; background-color:lavender;'><strong>$hostname</strong></td><td style='color:black; background-color:lightgreen; height: 40px;'><strong>$status</strong></td>" >> "$output_file"
+        echo "<tr>
+                 <td style='height: 40px; background-color:lavender;'>
+                 <strong>$hostname</strong>
+                 </td>
+                 <td style='color:black; background-color:lightgreen; height: 40px;'>
+                 <strong>$status</strong>
+                 </td>
+              </tr>" >> "$output_file"
         ##checking the disk utlization %
         #utilization_before=$(df -h / | awk 'NR==2{print $5}' | tr -d '%')
         #utilization_before=$(ssh -i "$private_key" "$hostname" "df -h / | awk 'NR==2{print \$5}' | tr -d '%'")
@@ -39,29 +50,38 @@ function check_server {
         echo "Disk Utilization After on $hostname: $utilization_after%"
         echo "<td style='background-color:lightgreen; height: 40px;'>$utilization_after%</td></tr>" >> "$output_file"
     else
-        echo "<tr style='background-color:lightcoral;'><td style='height: 40px; background-color:lavender;'><strong>$hostname</strong></td><td style='color:black; background-color:lightcoral; height: 40px;'><strong>$status</strong></td><td style='background-color:lightcoral; height: 40px;'>N/A</td><td style='background-color:lightcoral; height: 40px;'>N/A</td></tr>" >> "$output_file"
+         echo"<tr style='background-color:lightcoral;'>
+            <td style='height: 40px; background-color:lavender;'><strong>$hostname</strong></td>
+            <td style='color:black; background-color:lightcoral; height: 40px;'><strong>$status</strong></td>
+            <td style='background-color:lightcoral; height: 40px;'>N/A</td>
+            <td style='background-color:lightcoral; height: 40px;'>N/A</td>
+          </tr>" >> "$output_file"
+
     fi
 }
  
-# Create directory for the output file if it is not there
+
 mkdir -p /var/autoheal/housekeeping
  
 # Add a header to the HTML file
 echo "<html><head><title>Server Status Report</title></head><body>" > "$output_file"
 echo "<h1>FileSystem HouseKeeping Report</h1>" >> "$output_file"
-echo "<table border='1' style='border-collapse: collapse;'><tr><th style='color:black; background-color:yellow; width: 100px; height: 40px;'> &nbsp; IP Address &nbsp; </th><th style='color:black; background-color:yellow; width: 70px; height: 40px;'> &nbsp; Status &nbsp; </th><th style='color:black; background-color:yellow; width: 70px; height: 40px;'> &nbsp; Disk Utilization Before &nbsp; </th><th style='color:black; background-color:yellow; width: 70px; height: 40px;'> &nbsp; Disk Utilization After &nbsp; </th></tr>" >> "$output_file"
+echo "<table border='1' style='border-collapse: collapse;'>
+<tr>
+  <th style='color:black; background-color:yellow; width: 100px; height: 40px;'> &nbsp; IP Address &nbsp; </th>
+  <th style='color:black; background-color:yellow; width: 70px; height: 40px;'> &nbsp; Status &nbsp; </th>
+  <th style='color:black; background-color:yellow; width: 70px; height: 40px;'> &nbsp; Disk Utilization Before &nbsp; </th>
+  <th style='color:black; background-color:yellow; width: 70px; height: 40px;'> &nbsp; Disk Utilization After &nbsp; </th>
+</tr>" >> "$output_file"
+
  
- 
- 
-# Read ip_addresses from file and check status using a for loop
+
 for ip_address in $(cat "$input_file"); do
     check_server "$ip_address"
 done
- 
-# Add footer to the HTML file
+
 echo "</table></body></html>" >> "$output_file"
 echo "Filesystem Housekeeping report generated at: $output_file"
-
 
 # Email the report
 mail_subject="Filesystem Housekeeping report"
